cffce0497f0857283dfd1d715bf27994f4148141
Abhinav Garg <abhichhrp@gmail.com>
2025-05-17 23:58:46 +0530

    validate in routes

--- bites/routes/resturants.ts
+++ bites/routes/resturants.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import {validate} from "../middleware/validate.js"
+import { RestaurantSchema } from "../schemas/restaurant.js";
 const router = express.Router();
 
-router.get("/", async (req, res)=> {
+router.get("/", validate(RestaurantSchema), async (req, res)=> {
     const data = req.body 
     res.send("Hello World");
 })
