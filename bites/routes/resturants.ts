caea631bd0f4f8d8eccf5732c400af1ea6454be7
Abhinav Garg <abhichhrp@gmail.com>
2025-05-18 00:05:07 +0530

    redis setup

--- bites/routes/resturants.ts
+++ bites/routes/resturants.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import {validate} from "../middleware/validate.js"
 import { RestaurantSchema } from "../schemas/restaurant.js";
+import { intialiseRedisClient } from "../utils/clients.js";
 const router = express.Router();
 
 router.get("/", validate(RestaurantSchema), async (req, res)=> {
-    const data = req.body 
+    const data = req.body;
+    const client = await intialiseRedisClient()
     res.send("Hello World");
 })
 
